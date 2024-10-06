2da13123ec5faa01e57ad4ce7b89b5a345419e9c
VGA <109912901+VGA815@users.noreply.github.com>
2024-10-07 00:19:32 +0300

    commit

--- MessagesManagerDev/Controllers/MessagesController.cs
+++ MessagesManagerDev/Controllers/MessagesController.cs
@@ -8,6 +8,8 @@ using Newtonsoft.Json;
 using Microsoft.EntityFrameworkCore;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
+using Confluent.Kafka;
 namespace MessagesManagerDev.Controllers
 {
     [Route("api/msg")]
@@ -17,11 +19,13 @@ namespace MessagesManagerDev.Controllers
         private readonly IKafkaProducer _kafkaProducer;
         private ApplicationContext db;
         private ISubscriber sub;
-        public MessagesController(IKafkaProducer kafkaProducer, ApplicationContext context, IConnectionMultiplexer connectionMultiplexer)
+        private IConfiguration _configuration;
+        public MessagesController(IKafkaProducer kafkaProducer, ApplicationContext context, IConnectionMultiplexer connectionMultiplexer, IConfiguration configuration)
         {
             _kafkaProducer = kafkaProducer;
             db = context;
             sub = connectionMultiplexer.GetSubscriber();
+            _configuration = configuration;
         }
         [HttpPost]
         public async Task<IActionResult> PostMessage([FromBody] Message message)
@@ -55,6 +59,38 @@ namespace MessagesManagerDev.Controllers
             
             await sub.PublishAsync(message.ChatId.ToString(), JsonConvert.SerializeObject(rvalue));
             return Ok(rvalue);
+        }
+        [Route("img")]
+        [HttpPost]
+        public async Task<IActionResult> SendImg(string chatId, [FromForm] IFormFile file)
+        {
+            var chat = await db.Chats.FirstOrDefaultAsync(e => e.Id == Guid.Parse(chatId));
+            if (chat == null || file == null || file.Length <= 0) return BadRequest();
+            var filePath = Path.Combine(_configuration["actualPath"]!, file.FileName);
+            using (var filestream = System.IO.File.Create(filePath, 1024, FileOptions.SequentialScan))
+            {
+                await file.CopyToAsync(filestream);
+            }
+            var fileUrlBuilder = new UriBuilder(Request.GetEncodedUrl()) { Query = null, Fragment = null };
+            fileUrlBuilder.Path = Path.Combine(fileUrlBuilder.Path, file.FileName);
+            var rvalue = new
+            {
+                method = "publish",
+                @params = new
+                {
+                    channel = chatId,
+                    data = new
+                    {
+                        fileurl = fileUrlBuilder.Uri,
+                    }
+                }
+            };
+
+            using (var client = new HttpClient())
+            {
+                await client.PostAsync("http://centrifugo:8000/api", JsonContent.Create(rvalue));
+            }
+            return Ok(file);
         }
     }
 }
